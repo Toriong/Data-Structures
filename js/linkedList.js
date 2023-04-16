d8c2ce0b5b7c2691aac64b539f5aec6f2498ca3a
Gabe <gtorion97@gmail.com>
2023-04-15 23:05:23 -0500

    refactored javascript linkedList.js

--- js/linkedList.js
+++ js/linkedList.js
@@ -1,62 +1,68 @@
 class LinkedList {
-    constructor(arr) {
-      this.head = null;
-      this.tail = null;
-      this.nodes = {};
-      this.currentNode = "";
-      this.wasListCreated = false;
-      this.next = () => {};
-      this.getCurrentNode = () => {};
-      this.consoleCurrentNode = () => {};
-      this.createList(arr);
+  constructor(arr) {
+    this.head = null;
+    this.tail = null;
+    this.nodes = {};
+    this.currentNode = "";
+    this.next = () => { };
+    this.getCurrentNode = () => { };
+    this.consoleCurrentNode = () => { };
+    this.createList(arr);
+  }
+
+  createList(arr) {
+    if (Object.keys(this.nodes).length > 0) {
+      console.warn("List was created already.");
+      return;
     }
-  
-    createList(arr) {
-      if (this.wasListCreated) {
-        console.warn("List was created already.");
+    arr.forEach((val, index) => {
+      const nodeName = `node${index + 1}`;
+      const pointer = index === arr.length - 1 ? null : `node${index + 2}`;
+      const node = { val: val, pointer: pointer, nodeName: nodeName };
+      this.nodes[nodeName] = node;
+      if (index === 0) {
+        this.head = nodeName;
+      }
+      if (index === arr.length - 1) {
+        this.tail = nodeName;
+      }
+    });
+    this.currentNode = this.head;
+    this.next = () => {
+      const currentNodeObj = this.nodes[this.currentNode];
+      if (!currentNodeObj["pointer"]) {
+        console.warn("Reached the end of linked list.");
         return;
       }
-      arr.forEach((val, index) => {
-        const nodeName = `node${index + 1}`;
-        const pointer = index === arr.length - 1 ? null : `node${index + 2}`;
-        const node = { val: val, pointer: pointer, nodeName: nodeName };
-        this.nodes[nodeName] = node;
-        if (index === 0) {
-          this.head = nodeName;
-        }
-        if (index === arr.length - 1) {
-          this.tail = nodeName;
-        }
-      });
-      this.currentNode = this.head;
-      this.next = () => {
-        const currentNodeObj = this.nodes[this.currentNode];
-        if (!currentNodeObj["pointer"]) {
-          console.warn("Reached the end of linked list.");
-          return;
-        }
-        this.currentNode = currentNodeObj.pointer;
-      };
-  
-      this.getCurrentNode = () => this.nodes[this.currentNode];
-      this.consoleCurrentNode = () => {
-        console.log("current node: ", this.nodes[this.currentNode]);
-      };
-      this.wasListCreated = true;
-    }
+
+      // insert name of the next node into currentNode
+      this.currentNode = currentNodeObj.pointer;
+    };
+
+    this.getCurrentNode = () => this.nodes[this.currentNode];
+    this.consoleCurrentNode = () => {
+      console.log("current node: ", this.nodes[this.currentNode]);
+    };
+
   }
-  
-  const NUMS = [1, 2, 3, 4, 5];
-  const linkedList = new LinkedList(NUMS);
-  
-  const currentNode = linkedList.getCurrentNode();
-  
-  console.log("Before loop: ");
-  linkedList.consoleCurrentNode();
-  
-  console.log("Looping: ");
-  new Array(5).fill(0).forEach(() => {
-    linkedList.next();
-    linkedList.consoleCurrentNode();
-  });
-  
+}
+
+const NUMS = [1, 2, 3, 4, 5];
+const linkedList = new LinkedList(NUMS);
+
+const currentNode = linkedList.getCurrentNode();
+
+// console.log("Before loop: ");
+linkedList.consoleCurrentNode();
+
+// console.log("Looping: ");
+// new Array(5).fill(0).forEach(() => {
+//   linkedList.next();
+//   linkedList.consoleCurrentNode();
+// });
+linkedList.next()
+linkedList.consoleCurrentNode()
+
+linkedList.createList([1,2])
+
+
